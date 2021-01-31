65b87dcd5bfc9aa59d7afd6993f76ce180ed726d
deeckowy <58275642+deeckowy@users.noreply.github.com>
2021-01-31 19:59:19 +0100

    solution

---
+++ ArchitekturySystemowKomputerowych/attack/target-deeckowy/farm.c
@@ -0,0 +1,223 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_306()
+{
+    return 3284633944U;
+}
+
+void setval_120(unsigned *p)
+{
+    *p = 3251079496U;
+}
+
+unsigned getval_485()
+{
+    return 2425394264U;
+}
+
+unsigned addval_433(unsigned x)
+{
+    return x + 2455298951U;
+}
+
+unsigned addval_256(unsigned x)
+{
+    return x + 2425378878U;
+}
+
+unsigned getval_125()
+{
+    return 3284633928U;
+}
+
+unsigned getval_127()
+{
+    return 3284633928U;
+}
+
+void setval_186(unsigned *p)
+{
+    *p = 2425393240U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned getval_494()
+{
+    return 3677933193U;
+}
+
+unsigned getval_123()
+{
+    return 3526939017U;
+}
+
+unsigned addval_170(unsigned x)
+{
+    return x + 3526410889U;
+}
+
+void setval_210(unsigned *p)
+{
+    *p = 2463009214U;
+}
+
+unsigned addval_250(unsigned x)
+{
+    return x + 3286272360U;
+}
+
+unsigned addval_229(unsigned x)
+{
+    return x + 3675836809U;
+}
+
+unsigned addval_362(unsigned x)
+{
+    return x + 2425406089U;
+}
+
+void setval_382(unsigned *p)
+{
+    *p = 3221803401U;
+}
+
+unsigned getval_219()
+{
+    return 3286272329U;
+}
+
+void setval_242(unsigned *p)
+{
+    *p = 3348156041U;
+}
+
+unsigned addval_230(unsigned x)
+{
+    return x + 3284304289U;
+}
+
+unsigned addval_134(unsigned x)
+{
+    return x + 3526935177U;
+}
+
+unsigned getval_108()
+{
+    return 3767093339U;
+}
+
+unsigned addval_424(unsigned x)
+{
+    return x + 3373846921U;
+}
+
+unsigned addval_405(unsigned x)
+{
+    return x + 3380134537U;
+}
+
+unsigned addval_370(unsigned x)
+{
+    return x + 3372274057U;
+}
+
+void setval_264(unsigned *p)
+{
+    *p = 3221799625U;
+}
+
+unsigned getval_465()
+{
+    return 3526413961U;
+}
+
+void setval_381(unsigned *p)
+{
+    *p = 3514900556U;
+}
+
+void setval_438(unsigned *p)
+{
+    *p = 2429421920U;
+}
+
+unsigned addval_228(unsigned x)
+{
+    return x + 2430638408U;
+}
+
+unsigned getval_389()
+{
+    return 2430642504U;
+}
+
+void setval_158(unsigned *p)
+{
+    *p = 3286273352U;
+}
+
+unsigned addval_307(unsigned x)
+{
+    return x + 2464188744U;
+}
+
+void setval_401(unsigned *p)
+{
+    *p = 3281047961U;
+}
+
+unsigned addval_207(unsigned x)
+{
+    return x + 3227566729U;
+}
+
+unsigned getval_357()
+{
+    return 3375419017U;
+}
+
+unsigned addval_118(unsigned x)
+{
+    return x + 3525365385U;
+}
+
+void setval_119(unsigned *p)
+{
+    *p = 3380920968U;
+}
+
+void setval_373(unsigned *p)
+{
+    *p = 3526414729U;
+}
+
+void setval_268(unsigned *p)
+{
+    *p = 3375419017U;
+}
+
+void setval_435(unsigned *p)
+{
+    *p = 3286272328U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
